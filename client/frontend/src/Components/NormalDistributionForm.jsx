675774821f821865dbe4187a7404fbfa3824ce95
Darren Li <darren.li.1@stonybrook.edu>
2025-03-18 23:59:56 -0400

    Forms

---
+++ client/frontend/src/Components/NormalDistributionForm.jsx
@@ -0,0 +1,17 @@
+const NormalDistributionForm = (props) => {
+  return (
+    <div>
+      <div>
+        <span>Mean</span>
+        <input type="text"></input>
+        </div>
+        <div>
+        <span>Standard Deviation</span>
+        <input type="text"></input>
+      </div>
+    </div>
+  );
+}
+
+
+export default NormalDistributionForm;
---
+++ client/frontend/src/Components/UniformDistributionForm.jsx
@@ -0,0 +1,17 @@
+const UniformDistributionForm = (props) => {
+  return (
+    <div>
+      <div>
+      <span>Upper Bound</span>
+      <input type="text"></input>
+      </div>
+      <div>
+      <span>Lower Bound</span>
+      <input type="text"></input>
+      </div>
+    </div>
+  );
+}
+
+
+export default UniformDistributionForm;
